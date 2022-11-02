1f5dafed736e0bda8441fc44203428caab11b09d
AndStyver <andsty0125@skola.goteborg.se>
2022-11-02 14:10:23 +0100

    added eventsystem to Generator scene
    also tweaked menu

---
+++ Assets/Scripts/GameOverController.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOverController : MonoBehaviour
+{
+    public void ButtonRestart()
+    {
+        Debug.Log("Game Restarted");
+    }
+
+    public void ButtonBackToMenu()
+    {
+        Debug.Log("Returned to menu");
+    }
+}
