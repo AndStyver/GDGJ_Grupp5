1ac588354f01eba944f8b7a9b4f2a1e2009c1246
AndStyver <andsty0125@skola.goteborg.se>
2022-11-02 10:28:25 +0100

    added a story panel to main menu

--- Assets/Scripts/Menu/MenuScript.cs
+++ Assets/Scripts/Menu/MenuScript.cs
@@ -1,27 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class MenuScript : MonoBehaviour
 {
     [SerializeField] GameObject tutorialPanel;
     [SerializeField] GameObject creditsPanel;
+    [SerializeField] GameObject storyPanel;
+
     public void ButtonStart()
     {
+        SceneManager.LoadScene(1);
         Debug.Log("Game Started");
     }
 
     public void ButtonTutorial()
     {
         creditsPanel.SetActive(false);
+        storyPanel.SetActive(false);
         tutorialPanel.SetActive(!tutorialPanel.activeSelf);
     }
 
     public void ButtonCredits()
     {
         tutorialPanel.SetActive(false);
+        storyPanel.SetActive(false);
         creditsPanel.SetActive(!creditsPanel.activeSelf);
+    }
+
+    public void ButtonStory()
+    {
+        creditsPanel.SetActive(false);
+        tutorialPanel.SetActive(false);
+        storyPanel.SetActive(!storyPanel.activeSelf);
     }
 
     public void ButtonQuit()
