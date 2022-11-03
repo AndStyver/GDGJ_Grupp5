ad5ab94b47602b906de306d66f47d9c5669ab336
Thomas Enghult <thomas.enghult@gmail.com>
2022-11-03 10:59:24 +0100

    Update ScoreBoardController.cs

--- Assets/Scripts/ScoreBoardController.cs
+++ Assets/Scripts/ScoreBoardController.cs
@@ -1,19 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ScoreBoardController : MonoBehaviour
 {
     SortedDictionary<int, string> scoreboard;
 
+    string names;
+    string scores;
 
     void Start()
     {
         DontDestroyOnLoad(gameObject);
+        //names = gameObject.transform.GetChild(0).GetComponentsInChildren<TMPro>
     }
 
     void AddNewScore(ScoreHolder score)
     {
         scoreboard.Add(score.score, score.playerName);
+    }
+
+    void UpdateScore()
+    {
+        
     }
 }
