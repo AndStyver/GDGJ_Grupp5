cb5d57c32c2c71ba7dabbfb3319610adaaffda14
Thomas Enghult <thomas.enghult@gmail.com>
2022-11-03 14:22:23 +0100

    Updated sorting in scoreController

--- Assets/Scripts/ScoreBoardController.cs
+++ Assets/Scripts/ScoreBoardController.cs
@@ -26,7 +26,17 @@ public class ScoreBoardController : MonoBehaviour
 
     public void AddNewScore(ScoreHolder score)
     {
-        scoreboard.Add(score.score, score.playerName);
+        score.score  = -score.score; //invert for sorting
+        if (score.score == 0)
+        {
+            Debug.Log("Insufficient Score, no score added");
+            return;
+        }
+
+        if (scoreboard.ContainsKey(score.score))
+            scoreboard[score.score] += ", " + score.playerName;
+        else
+            scoreboard.Add(score.score, score.playerName);
     }
 
     public void UpdateScore()
@@ -43,7 +53,7 @@ public class ScoreBoardController : MonoBehaviour
         scores.text = "";
         foreach ( var score in scoreboard)
         {
-            scores.text += (score.Key + "\n");
+            scores.text += (Mathf.Abs(score.Key) + "\n");
             names.text += (score.Value + "\n");
             Debug.Log(names.text);
             Debug.Log(scores.text);
